f47b6b1880df920d2265e3912f95964707b14835
alex haines <alexhaines@alexs-MacBook-Pro.local>
2017-04-08 11:37:07 -0700

    convert html to handlebars file

--- testEmail.js
+++ testEmail.js
@@ -1,3 +1,14 @@
 var sendEmail = require('./server/services/email/sendEmail.js')
 
-sendEmail('hainesaxiv@gmail.com');
+var lead = {
+    name: 'alex',
+    address: '261 gabarda way',
+    phone: '555-5555',
+    roofType: 'tile',
+    ownership: 'own',
+    residenceType: 'single family',
+    financePref: 'lease',
+    apptDateTime: '5/5/2017'
+}
+
+sendEmail('tima.haines@gmail.com', lead);
