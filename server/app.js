25fd0b8b315c8cc05f1cfe16f58c065098f07c9d
Charles Buhler <charlesbuhler@gmail.com>
2017-04-08 09:39:01 -0700

    Adding chat server with socket.io:wq

--- server/app.js
+++ server/app.js
@@ -1,11 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+// TODO: Extract chat client, services, etc.
+
+var ChatClient = function() {
+  
+  this.getResponseFromMessage = function(message) {
+    return 'you said ' + message + '. ' + 'I say hi!';
+  }
+};
+
+var chatClient = new ChatClient();
+
+// TODO: Replace with react app
 app.get('/', function(req, res){
-  res.send('<h1>I love QUAL-E</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(message){
+    
+    var response = chatClient.getResponseFromMessage(message);
+
+    io.emit('chat message', response);
+  });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
