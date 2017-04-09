2cf1defa0e8e3a7a6db1b847879d378b0d66414c
alex haines <alexhaines@alexs-MacBook-Pro.local>
2017-04-08 17:01:31 -0700

    added render chat script

---
+++ client/src/renderChat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.js';
+
+var chat = document.createElement("div");
+
+console.log('whhhattt uppp');
+
+document.body.appendChild(chat);
+
+ReactDOM.render(<App />, chat);
+
